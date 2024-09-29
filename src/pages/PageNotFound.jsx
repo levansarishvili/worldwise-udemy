180c9fbd861010c1f178e077416a4c76d681f687
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-09-29 21:24:30 +0400

    init: start worldwise app

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+// import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      {/* <PageNav /> */}
+      Page not Found 😞
+    </div>
+  );
+}
+
+export default PageNotFound;
