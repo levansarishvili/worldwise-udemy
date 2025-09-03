2b853923efaab3a635e509bae6ee2ebebb9228a0
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-09-03 11:55:10 +0400

    feat: create sidebar component with nested routes for cities and countries

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+  console.log(countries);
+
+  return (
+    <ul className={styles.CountriesList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
