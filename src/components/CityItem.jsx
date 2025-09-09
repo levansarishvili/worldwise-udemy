88c2ea6d282afabe5967386cc098bdc2cf954e5b
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-09-09 21:53:48 +0400

    feat: implement functionality to delete city

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -10,8 +10,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
   const { id, cityName, emoji, date, position } = city;
+
+  function handleDelete(e) {
+    e.preventDefault();
+    deleteCity(city.id);
+  }
 
   return (
     <li>
@@ -24,7 +29,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={(e) => handleDelete(e)}>
+          &times;
+        </button>
       </Link>
     </li>
   );
